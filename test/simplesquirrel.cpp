e112aca52a2b197b5a72b339f4b129e09cec5072
西风逍遥游 <sunxfancy@gmail.com>
2021-09-30 19:53:22 -0700

    add simplesquirrel unit test

--- test/simplesquirrel.cpp
+++ test/simplesquirrel.cpp
@@ -1,9 +1,68 @@
+#include <iostream>
+
 #include "ScriptTest.h"
 
 extern const char* simplesquirrel_content;
 UnitSciptTest(SimpleSquirrelTest, simplesquirrel_content);
 
-TEST_F(SimpleSquirrelTest, ExportClass) {
-  EXPECT_TRUE(vm.callFunc(vm.findFunc("exportClass"), vm).toBool());
+TEST_F(SimpleSquirrelTest, ExportClass) { EXPECT_TRUE(vm.callFunc(vm.findFunc("exportClass"), vm).toBool()); }
+
+class A {
+public:
+    SSQ_REG
+    A() {
+        data = new char[32];
+        size = 0;
+        std::cerr << "A()" << std::endl;
+    }
+    ~A() {
+        delete data;
+        std::cerr << "~A()" << std::endl;
+    }
+    void setString(std::string name) {
+        if (name.size() < 32) {
+            strncpy(data, name.c_str(), name.size());
+            data[name.size()] = '\0';
+            size              = name.size();
+        }
+    }
+    char* data;
+    int   size;
+};
+
+class B {
+public:
+    SSQ_REG
+    B() { std::cerr << "B()" << std::endl; }
+    ~B() { std::cerr << "~B()" << std::endl; }
+
+    // void print() { std::cerr << a->data << std::endl; }
+    // void setA(A* a) { this->a = a; }
+    // A* a;
+    void          setA(ssq::Instance a) { this->a = a; }
+    void          print() { std::cerr << a.to<A*>()->data << std::endl; }
+    ssq::Instance a;
+};
+
+void A::expose(ssq::Table& table) {
+    auto cls = table.addClass("A", ssq::Class::Ctor<A()>());
+    expose(cls);
 }
 
+void A::expose(ssq::Class& cls) { cls.addFunc("setString", &A::setString); }
+
+void B::expose(ssq::Table& table) {
+    auto cls = table.addClass("B", ssq::Class::Ctor<B()>());
+    expose(cls);
+}
+
+void B::expose(ssq::Class& cls) {
+    cls.addFunc("setA", &B::setA);
+    cls.addFunc("print", &B::print);
+}
+
+TEST_F(SimpleSquirrelTest, RefTest) {
+    A::expose(vm);
+    B::expose(vm);
+    EXPECT_TRUE(vm.callFunc(vm.findFunc("refTest"), vm).toBool());
+}
--- test/simplesquirrel.nut
+++ test/simplesquirrel.nut
@@ -1,4 +1,19 @@
 
 function exportClass() {
+    if (eve.Window == null) return false;
+    return true;
+}
+
+function get() {
+    local a = A();
+    local b = B();
+    a.setString("print");
+    b.setA(a);
+    return b;
+}
+
+function refTest() {
+    local a = get();
+    a.print();
     return true;
 }
