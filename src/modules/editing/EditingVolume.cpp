e82ebfed984ec288c34fede966a0a4e9e59003f7
西风逍遥游 <sunxfancy@gmail.com>
2026-08-31 00:58:08 +0800

    fix(editing): satisfy strict volume formatting

--- src/modules/editing/EditingVolume.cpp
+++ src/modules/editing/EditingVolume.cpp
@@ -4,12 +4,17 @@ namespace eve::editing {
 
 void EditVolume::include(int x, int y, int z) {
     if (empty()) {
-        minX = maxX = x; minY = maxY = y; minZ = maxZ = z;
+        minX = maxX = x;
+        minY = maxY = y;
+        minZ = maxZ = z;
         return;
     }
-    if (x < minX) minX = x; if (x > maxX) maxX = x;
-    if (y < minY) minY = y; if (y > maxY) maxY = y;
-    if (z < minZ) minZ = z; if (z > maxZ) maxZ = z;
+    if (x < minX) minX = x;
+    if (x > maxX) maxX = x;
+    if (y < minY) minY = y;
+    if (y > maxY) maxY = y;
+    if (z < minZ) minZ = z;
+    if (z > maxZ) maxZ = z;
 }
 
 void EditVolume::include(const EditVolume& other) {
@@ -20,7 +25,10 @@ void EditVolume::include(const EditVolume& other) {
 
 EditRegion EditVolume::xzRegion() const {
     EditRegion region;
-    if (!empty()) { region.include(minX, minZ); region.include(maxX, maxZ); }
+    if (!empty()) {
+        region.include(minX, minZ);
+        region.include(maxX, maxZ);
+    }
     return region;
 }
 
